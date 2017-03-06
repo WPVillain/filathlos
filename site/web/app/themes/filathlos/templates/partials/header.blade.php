f6176a9eb1d2e0d0c3caaf9c98677f565d0b8ebf
Jasper Frumau <jasper@imagewize.com>
2017-03-06 12:42:36 +0300

    added pull-right class

--- site/web/app/themes/filathlos/templates/partials/header.blade.php
+++ site/web/app/themes/filathlos/templates/partials/header.blade.php
@@ -12,7 +12,7 @@
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         @if (has_nav_menu('primary_navigation'))
-          {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'navbar-nav mr-auto']) !!}
+          {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'navbar-nav nav pull-right']) !!}
         @endif
       </div>
     </nav>
