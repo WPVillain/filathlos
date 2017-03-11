49cad19bc2fa1bfbea445e180325f660d915021f
Jasper Frumau <jasper@imagewize.com>
2017-03-11 18:12:50 +0300

    added dummy columns

--- site/web/app/themes/filathlos/templates/partials/footer.blade.php
+++ site/web/app/themes/filathlos/templates/partials/footer.blade.php
@@ -1,5 +1,24 @@
 <footer class="content-info">
   <div class="container">
-    @php(dynamic_sidebar('sidebar-footer'))
-  </div>
+    <div class="row">
+	    <div class="col-md-4">
+		     @php(dynamic_sidebar('sidebar-footer'))
+		</div>
+	    <div class="col-md-4">
+		    <ul>  
+				<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+				<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+				<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+				<li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+			</ul>
+	  </div>
+	  <div class="col-md-4">
+		    <ul>  
+				<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+				<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+				<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+				<li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+			</ul>
+	  </div>
+	</div>
 </footer>
