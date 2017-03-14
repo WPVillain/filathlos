972187507a7b5d9719f7c4577cf12a89711b2360
Jasper Frumau <jasper@imagewize.com>
2017-03-14 11:36:27 +0300

    added new image size for slider

--- site/web/app/themes/filathlos/src/setup.php
+++ site/web/app/themes/filathlos/src/setup.php
@@ -56,6 +56,8 @@ add_action('after_setup_theme', function () {
      */
     add_theme_support('post-thumbnails');
 
+    add_image_size( 'b4-slider', 2000, 1500);
+
     /**
      * Enable HTML5 markup support
      * @link https://developer.wordpress.org/reference/functions/add_theme_support/#html5
